0410bf8237010041e74c96d3a75f07b15dea4e51
thinkpad <me@thinkpad.com>
2025-09-20 17:29:19 +0900

    health endpoint

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,16 +1,20 @@
 use axum::{
-    response::IntoResponse, routing::get, Router
+    http::StatusCode, response::IntoResponse, routing::get, Json, Router
 };
+use serde::Serialize;
 
 pub async fn init() {
-    // build our application with a single route
     let app = Router::new().route("/health", get(health));
-
-    // run our app with hyper, listening globally on port 3000
+    
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
+#[derive(Serialize)]
+struct HealthStatus {
+    status: &'static str,
+}
+
 async fn health() -> impl IntoResponse {
-    "healthy"
+    (StatusCode::OK, Json(HealthStatus { status: "healthy" }))
 }
